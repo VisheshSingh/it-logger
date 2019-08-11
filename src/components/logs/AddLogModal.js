4bf1d0a8304333d30dce265054d1b57a49529740
VisheshSingh <visheshsingh16@gmail.com>
2019-08-11 18:04:21 -0500

    AddLogModal component

---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+
+const AddLogModal = () => {
+  const [message, setMessage] = useState('');
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState('');
+
+  const onSubmit = () => {
+    console.log(message, attention, tech);
+  };
+
+  return (
+    <div style={modalStyle} id="add-log-modal" className="modal">
+      <div className="modal-content">
+        <h4>Enter System Log</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="message"
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+            />
+            <label htmlFor="message" className="active">
+              Log Message
+            </label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field">
+            <select
+              className="browser-default"
+              name="tech"
+              value={tech}
+              onChange={e => setTech(e.target.value)}
+            >
+              <option value="" disabled>
+                Select a Technician
+              </option>
+              <option value="Stephen Frein">Stephen Frein</option>
+              <option value="Dwight Schrute">Dwight Schrute</option>
+              <option value="Pam Beesley">Pam Beesley</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field">
+            <label>
+              <input
+                type="checkbox"
+                className="filled-in"
+                checked={attention}
+                value={attention}
+                onChange={e => setAttention(!attention)}
+              />
+              <span>Needs Attention</span>
+            </label>
+          </div>
+        </div>
+      </div>
+
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          className="modal-close blue waves-effect waves-blue btn"
+        >
+          Enter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const modalStyle = {
+  width: '75%',
+  height: '75%'
+};
+
+export default AddLogModal;
