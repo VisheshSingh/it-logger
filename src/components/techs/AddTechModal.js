a12d7b6c9adfdee560526e680289ed32af753e1b
VisheshSingh <visheshsingh16@gmail.com>
2019-08-17 09:55:41 -0500

    fix warnings

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 const AddTechModal = () => {
